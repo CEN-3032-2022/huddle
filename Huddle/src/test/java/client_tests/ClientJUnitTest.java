8989838cba1b7306f68da5a7d6b5a27ead3cc8f4
Brandon White <Bmw54@students.uwf.edu>
2022-03-02 16:19:19 -0600

    Trying to fix error

---
+++ Huddle/src/test/java/client_tests/ClientJUnitTest.java
@@ -0,0 +1,21 @@
+package client_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+// Temporary class to ensure JUnit functionality works
+// REMOVE LATER
+public class ClientJUnitTest {
+	
+	@Test
+	public void testTrue() {
+		assert(true);
+	}
+	
+	@Test
+	public void testNotFalse() {
+		assert(!false);
+	}
+
+}
