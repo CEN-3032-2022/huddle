351293a26a8060e8329fe35c8ab47d89a27d6069
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-03-06 17:50:04 -0600

    Finished styling create account page for default theme

--- Huddle/src/main/java/client/AccountCreationController.java
+++ Huddle/src/main/java/client/AccountCreationController.java
@@ -18,10 +18,15 @@ public class AccountCreationController {
 	@FXML Text req3;
 	@FXML Text taken;
 	@FXML int pass;
+	
+	public String LIGHT_RED = "#f24b4e";
+	public String LIGHT_GREEN = "#6eff72";
+	
     @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("/Group7/Huddle/UserInterface/Login");
     }
+    
     @FXML
     private void handleOnKeyReleasedUsrName() throws IOException{
     	if(userName.getText().length() > 20) {
@@ -29,6 +34,7 @@ public class AccountCreationController {
     		userName.positionCaret(20);
     	}
     }
+    
     @FXML
     private void limit() {
     	if(Q2.getText().length() > 30) {
@@ -40,6 +46,7 @@ public class AccountCreationController {
     		Q1.positionCaret(30);
     	}
     }
+    
     @FXML
     private void handleOnKeyReleasedPassword() throws IOException{
     	if(password.getText().length()>20) {
@@ -56,47 +63,52 @@ public class AccountCreationController {
     		if(App.getUserAgentStylesheet().contains("2"))
     			req1.setVisible(false);
     		else
-    			req1.setFill(Color.DARKGREEN);
+    			req1.setFill(Color.web(LIGHT_GREEN));
     	} else {
     		if(App.getUserAgentStylesheet().contains("2"))
     			req1.setVisible(true);
     		else
-    			req1.setFill(Color.BLACK);
+    			req1.setFill(Color.web(LIGHT_RED));
     	}
     	
     	if(matcher2.find()) {
     		if(App.getUserAgentStylesheet().contains("2"))
     			req2.setVisible(false);
     		else
-    			req2.setFill(Color.DARKGREEN);
+    			req2.setFill(Color.web(LIGHT_GREEN));
     	} else {
     		if(App.getUserAgentStylesheet().contains("2"))
     			req2.setVisible(true);
     		else
-    			req2.setFill(Color.BLACK);
+    			req2.setFill(Color.web(LIGHT_RED));
     	}
     	
     	if(matcher3.find()) {
     		if(App.getUserAgentStylesheet().contains("2"))
     			req3.setVisible(false);
     		else
-    			req3.setFill(Color.DARKGREEN);
+    			req3.setFill(Color.web(LIGHT_GREEN));
     	} else {
     		if(App.getUserAgentStylesheet().contains("2"))
     			req3.setVisible(true);
     		else
-    			req3.setFill(Color.BLACK);
+    			req3.setFill(Color.web(LIGHT_RED));
     	}
-    	
     }
+    
     @FXML
-    private void Confirm() throws IOException{
-    	if((userName.getText().length()>=5&&userName.getText().length()<=20)) {
-    		if(App.getUserAgentStylesheet().contains("1"))
-    			taken.setFill(Color.RED);
+    private void createAccount() throws IOException{
+    	if((userName.getText().length() >=5 && userName.getText().length() <= 20)) {
+    		taken.setVisible(false);
+    	}
+    	else {
+    		taken.setFill(Color.web(LIGHT_RED));
     		taken.setVisible(true);
     	}
-    	else
-    		switchToPrimary();
+    }
+    
+    @FXML
+    private void returnToLoginPage() throws IOException{
+    	switchToPrimary();
     }
 }
