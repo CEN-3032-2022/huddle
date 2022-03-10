98b95b3590c0b3fb0c203d48ef5a0a339d3ddc5f
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-03-09 19:43:03 -0600

    Implemented the client communication class as a singleton

--- Huddle/src/main/java/client/HomeScreenController.java
+++ Huddle/src/main/java/client/HomeScreenController.java
@@ -25,12 +25,11 @@ public class HomeScreenController {
     @FXML
     private void switchToWall() throws IOException {
     	String value="";
-    	ClientCommunication sut = new ClientCommunication();
 		JSONObject JSON = new JSONObject();
 		JSON.put("type", "HonkList");
 		JSON.put("isTest", false);
-		sut.sendJSONRequestToServer(JSON);
-		value=sut.getServerHonkListJSONResponse();
+    	ClientCommunication.getInstance().sendJSONRequestToServer(JSON);
+		value=ClientCommunication.getInstance().getServerHonkListJSONResponse();
 		JSONArray Arr=new JSONArray(value);
 		GridPane tPane=new GridPane();
 		System.out.println(Arr.getJSONObject(0).toString());
