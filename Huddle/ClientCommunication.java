bc2ed0d72cd284a23cf2882d19aa5fce20d5f7a2
epicninjapenguin1 <86076449+epicninjapenguin1@users.noreply.github.com>
2022-03-09 20:51:49 -0600

    Completed post honk and login controllers

---
+++ Huddle/ClientCommunication.java
@@ -0,0 +1,70 @@
+package client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
+public class ClientCommunication {
+
+    public static final int HUDDLE_PORT = 8888;
+	
+	private Socket clientSocket;
+	private Scanner serverInput;
+	private PrintWriter serverOutput;
+	
+	public ClientCommunication() {
+		try {
+			clientSocket = new Socket("localhost", HUDDLE_PORT);
+			setUpCommunications();
+		}
+		catch (IOException e) {
+			System.out.println("Could not connect to the server");
+			e.printStackTrace();
+		}
+	}
+	
+	public void setUpCommunications() throws IOException {
+		InputStream instream = clientSocket.getInputStream();
+		OutputStream outstream = clientSocket.getOutputStream();
+		this.serverInput = new Scanner(instream, StandardCharsets.UTF_8);
+		this.serverOutput = new PrintWriter(outstream);
+	}
+	
+	public void closeCommunications() {
+    	try {
+    		serverInput.close();
+    		serverOutput.close();
+    		clientSocket.close();
+    	} catch (Exception e) {
+    		System.out.println("Could not close connections");
+    		e.printStackTrace();
+    	}
+	}
+	
+	public JSONObject getServerJSONResponse() {
+		String jsonResponse = serverInput.nextLine();
+//	    System.out.println("Recieved From Server: " + jsonResponse.toString());
+		return new JSONObject(jsonResponse);
+	}
+	public String getServerUsersJSONResponse() {
+		String jsonResponse = serverInput.nextLine();
+//	    System.out.println("Recieved From Server: " + jsonResponse.toString());
+		return jsonResponse;
+	}
+	public String getServerHonkListJSONResponse() {
+		String jsonResponse = serverInput.nextLine();
+//	    System.out.println("Recieved From Server: " + jsonResponse.toString());
+		return jsonResponse;
+	}
+	public void sendJSONRequestToServer(JSONObject jsonServerRequest) {
+//	    System.out.println("Sending To Server: " + jsonServerRequest.toString());
+		serverOutput.println(jsonServerRequest.toString());
+		serverOutput.flush();
+	}
+}
---
+++ Huddle/ServerRequest.java
@@ -0,0 +1,139 @@
+package Server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ServerRequest implements Runnable {
+	
+	private Socket huddleSocket;
+	private Scanner clientInput;
+	private PrintWriter clientOutput;
+	
+	public ServerRequest(Socket huddleSocket) {
+		this.huddleSocket = huddleSocket;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			try {
+				setUpCommunications();
+	            doRequests();           
+			}
+			finally { huddleSocket.close(); }
+		}
+		catch (IOException exception) { exception.printStackTrace(); }
+	}
+	
+	public void doRequests() throws IOException {
+		while (true) { 
+			if (!clientInput.hasNextLine()) return;
+			handleServerRequest(getClientJSONResponse());
+		}
+	}
+	
+	public void handleServerRequest(JSONObject jsonRequest) {
+		
+		// Test responses to test requests
+		if(jsonRequest.getString("type").equals("testRequest")) {
+			sendJSONResponseToClient(getTestJSONResponse());
+		}
+		else if(jsonRequest.getString("type").equals("userData")) {
+			sendJSONResponseToClient(getTestUserDataJSONResponse());
+		}
+		else if(jsonRequest.getString("type").equals("HonkList")) {
+			sendJSONHonkResponseToClient(getHonkList().toString());
+		} else if(jsonRequest.getString("type").equals("UserList")) {
+			sendJSONUsersResponseToClient(getUsers().toString());
+		}
+		
+		return;
+	}
+	
+	private void sendJSONHonkResponseToClient(String honkList) {
+		// TODO Auto-generated method stub
+		clientOutput.println(honkList);
+		clientOutput.flush();
+	}
+	
+	private void sendJSONUsersResponseToClient(String userList) {
+		// TODO Auto-generated method stub
+		clientOutput.println(userList);
+		clientOutput.flush();
+	}
+
+	public JSONObject getClientJSONResponse() {
+		String jsonResponse = clientInput.nextLine();
+//		System.out.println("Recieved From Client: " + jsonResponse);
+		return new JSONObject(jsonResponse);
+	}
+	
+	public void sendJSONResponseToClient(JSONObject jsonClientResponse) {
+//	    System.out.println("Sending To Client: " + jsonClientResponse.toString());
+		clientOutput.println(jsonClientResponse.toString());
+		clientOutput.flush();
+	}
+	
+	public void setUpCommunications() throws IOException {
+		InputStream instream = huddleSocket.getInputStream();
+		OutputStream outstream = huddleSocket.getOutputStream();
+		this.clientInput = new Scanner(instream, StandardCharsets.UTF_8);
+        this.clientOutput = new PrintWriter(outstream);
+	}
+	
+	// ---------- Temporary Testing Methods	------------
+	public JSONObject getTestUserDataJSONResponse() {
+		JSONObject testJSONResponse = new JSONObject();
+		testJSONResponse.put("type", "userDataResponse");
+		testJSONResponse.put("isTest", true);
+		return testJSONResponse;
+	}
+	
+	public JSONObject getTestJSONResponse() {
+		JSONObject testJSONResponse = new JSONObject();
+		testJSONResponse.put("type", "testResponse");
+		testJSONResponse.put("isTest", true);
+		testJSONResponse.put("number", 12345);
+		JSONArray jsonArray = new JSONArray();
+		for(int i = 0; i < 10; ++i) {
+			jsonArray.put(i);
+		}
+		testJSONResponse.put("array", jsonArray);
+		return testJSONResponse;
+	}
+	public JSONArray getHonkList() {
+		JSONArray jsonArray = new JSONArray();
+		for(int i = 0; i < 10; ++i) {
+			JSONObject Response = new JSONObject();
+			Response.put("id", 1);
+			Response.put("content","Hi");
+			Response.put("date", "1/1/11");
+			Response.put("UserName", "Test"+i);
+			jsonArray.put(Response);
+		}
+		return jsonArray;
+	}
+	
+	
+	public JSONArray getUsers() {
+			JSONArray jsonArray = new JSONArray();
+			JSONObject Response = new JSONObject();
+			
+			Response.put("id", 1);
+			Response.put("UserName", "daniel");
+			Response.put("Password", "password");
+			jsonArray.put(Response);
+	
+		return jsonArray;
+	}
+	// ------------ End Of Testing Methods -------------
+	
+}
