65b04a537aa7d4bbe1dba1e4cba4a0f025ac2580
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-03-10 17:55:49 -0600

    Added file data retriever class and text files

---
+++ Huddle/src/main/java/Server/FileDataRetriever.java
@@ -0,0 +1,28 @@
+package Server;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class FileDataRetriever {
+
+	JSONObject getUserByUsername(String username) {
+		
+		return null;
+	}
+	
+	JSONArray getAllUsers() {
+		
+		return null;
+	}
+	
+	public JSONArray getAllHonks() {
+		
+		return null;
+	}
+	
+	public JSONArray getAllHonksFromUser(String username) {
+		
+		return null;
+	}
+	
+}
