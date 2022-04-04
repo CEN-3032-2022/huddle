5887ec9911b6467a2a6c9f8cd9ffdd134522a817
Brandon White <Bmw54@students.uwf.edu>
2022-04-04 18:33:00 -0500

    spacing for easier commenting out

--- Huddle/src/test/java/server_tests/dbTest.java
+++ Huddle/src/test/java/server_tests/dbTest.java
@@ -13,25 +13,29 @@ import Database.UserAccess;
 public class dbTest {
 	@Test
 	public void test1(){
-	try {
-	Connection connect = ConnectionFactory.getConnection();
-		
-	PreparedStatement preparedStatement = connect.prepareStatement("DROP TABLE APP.USERS");
-	preparedStatement.execute();
-		
-		PreparedStatement preparedStatement2 = connect.prepareStatement("CREATE TABLE APP.USERS(ID INTEGER NOT NULL GENERATED "
-				+ "ALWAYS AS IDENTITY,"
-				+ " USERNAME VARCHAR(255), PASSWORD VARCHAR(255), BIOGRAPHY VARCHAR(255), PRIMARY KEY (ID))");
-		preparedStatement2.execute();
 
-	UserAccess userAccess = new UserAccess();
-    User user = new User.UserBuilder("user2", "Password2")
-           .userBiography("Bio2")
-            .build();
-    userAccess.createUser(user);
-    userAccess.readAllUsers();
-	}catch(SQLException e) { 
-		System.out.print(e.getErrorCode()+"\n"+e.getCause());
+		try {
+
+			Connection connect = ConnectionFactory.getConnection();
+
+			PreparedStatement preparedStatement = connect.prepareStatement("DROP TABLE APP.USERS");
+			preparedStatement.execute();
+			PreparedStatement preparedStatement2 = connect.prepareStatement("CREATE TABLE APP.USERS(ID INTEGER NOT NULL GENERATED "
+					+ "ALWAYS AS IDENTITY,"
+					+ " USERNAME VARCHAR(255), PASSWORD VARCHAR(255), BIOGRAPHY VARCHAR(255), PRIMARY KEY (ID))");
+			preparedStatement2.execute();
+
+			UserAccess userAccess = new UserAccess();
+			User user = new User.UserBuilder("user2", "Password2")
+					.userBiography("Bio2")
+					.build();
+			userAccess.createUser(user);
+			userAccess.readAllUsers();
+		}
+
+		catch(SQLException e) { 
+			System.out.print(e.getErrorCode()+"\n"+e.getCause());
+		}
 	}
-	}
+
 }
