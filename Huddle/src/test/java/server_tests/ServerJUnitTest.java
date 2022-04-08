fd01472b3bc266550ad613d5ef6f058009a08474
Brandon White <Bmw54@students.uwf.edu>
2022-04-08 15:56:40 -0500

    usr search

--- Huddle/src/test/java/server_tests/ServerJUnitTest.java
+++ Huddle/src/test/java/server_tests/ServerJUnitTest.java
@@ -7,29 +7,37 @@ import org.junit.Test;
 
 import client.ClientCommunication;
 import client.Honk;
+import client.User;
+import client.UserRepositoryImp;
 
 // Temporary class to ensure JUnit functionality works
 // REMOVE LATER
 public class ServerJUnitTest {
 	
 	@Test
-	public void testTrue() {
-		assert(true);
+	public void testFollow() {
+		JSONObject JSON = new JSONObject();
+		JSON.put("type", "user");
+		JSON.put("request", "followUser");
+		JSON.put("userFollowing","Bmw54");
+		JSON.put("userToFollow", "benjamin");
+		ClientCommunication.getInstance().sendJSONRequestToServer(JSON);
+		JSONObject x = ClientCommunication.getInstance().getServerJSONResponse();
+		boolean y = x.getBoolean("isSuccess");
+		assert(y==true);
 	}
 	@Test
-	public void HonkTest() {
-//		String value = null;
-//		ClientCommunication sut = new ClientCommunication();
-//		JSONObject testJSON = new JSONObject();
-//		testJSON.put("type", "HonkList");
-//		testJSON.put("isTest", false);
-//		sut.sendJSONRequestToServer(testJSON);
-//		assert((value=sut.getServerHonkListJSONResponse())!=null);
-//		System.out.println(value);
+	public void testSearchUsr() {
+		UserRepositoryImp x = new UserRepositoryImp();
+		User usr = x.getUserByUsername("Bmw54");
+		assert(usr.getUsername().equals("Bmw54"));
 	}
 	@Test
-	public void testNotFalse() {
-		assert(!false);
+	public void testLike() {
+		Honk honk1 = new Honk();
+		
+		honk1.setNumLikes(honk1.getNumLikes()+1);
+		
+		assert(honk1.getNumLikes() == 1);
 	}
-
 }
