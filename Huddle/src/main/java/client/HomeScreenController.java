1083681ed601f036b4d915d8da988483b9cee3a4
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-04-13 12:23:12 -0500

    Made honks look better

--- Huddle/src/main/java/client/HomeScreenController.java
+++ Huddle/src/main/java/client/HomeScreenController.java
@@ -9,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -129,6 +130,7 @@ public class HomeScreenController{
 		
 		for(int i=0;i<honks.size();i++) {
 			GridPane honk = new GridPane();
+			honk.setPadding(new Insets(3));
 			honk.getStyleClass().add("honk");
 			honk.setHgap(5);
 			ColumnConstraints col2 = new ColumnConstraints();
