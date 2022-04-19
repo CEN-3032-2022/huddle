07ac9685476bf4e1d842ca4778d2426b2f1d7f33
Ben Shinnick <shinnickbenjamin@gmail.com>
2022-04-19 12:08:49 -0500

    Updated change bio page styling

--- Huddle/src/main/java/client/bioChangeController.java
+++ Huddle/src/main/java/client/bioChangeController.java
@@ -9,12 +9,28 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class bioChangeController {
-    @FXML
+	private int MAX_BIO_CHAR_LENTH = 500;
+	@FXML TextArea writeBioTextArea;
+
+	@FXML
     private void switchToHome() throws IOException {
         App.setRoot("/fxml/HomeScreenUsr");
     }
     @FXML
-    private void change() throws IOException{
-    	
+    private void changeBio() throws IOException{
+    	System.out.println("Change Bio");
     }
+    @FXML
+    private void handleOnKeyReleasedBio() throws IOException{
+    	clearExtraCharsFromTextArea(writeBioTextArea, MAX_BIO_CHAR_LENTH);
+    }
+    
+    private void clearExtraCharsFromTextArea(TextArea textArea, int maxCharLength) {
+    	if(textArea.getText().length() > maxCharLength) {
+    		textArea.setText(textArea.getText().substring(0, maxCharLength));
+    		textArea.positionCaret(maxCharLength);
+    	}
+    }
+    
+    
 }
